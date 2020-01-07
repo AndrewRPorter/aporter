601ca9023c65724d9a2af9fce4ccdd4437653261
AndrewRPorter <pavelow53.andrew@gmail.com>
2020-01-07 12:46:55 -0500

    moved experience code over and added icon

--- app/views/main_view.py
+++ app/views/main_view.py
@@ -5,10 +5,16 @@ from ..tools import crawler
 main_bp = Blueprint("main", __name__, url_prefix="/")
 
 
-@main_bp.route("/", methods=["GET", "POST"])
+@main_bp.route("/", methods=["GET"])
 def index() -> Response:
     """Defines the main website view"""
     return render_template("index.html")
+
+
+@main_bp.route("/experience", methods=["GET"])
+def experience() -> Response:
+    """Defines the experience website view"""
+    return render_template("experience.html")
 
 
 @main_bp.route("/projects", methods=["GET", "POST"])
